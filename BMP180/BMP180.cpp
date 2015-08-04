06a881cd9bddae50a02958518b7090e5f2225e40
Kyle Veilleux <kylebv@gmail.com>
2015-08-04 17:19:55 -0400

    Updated to correct BMP180 code as well as initial Atmel studio project for the ATMega328p, and the Fleury I2C interface

--- BMP180/BMP180.cpp
+++ BMP180/BMP180.cpp
@@ -7,31 +7,110 @@
 
 
 #include "BMP180.h"
+#include <math.h>
 
 void BMP180::Setup()
 {
-	BMP180_GetCalibrationData();
+	oversampling_setting = 0;
+
+	// We need a longer delay based on the OSS chosen above for more readings
+	oss_delay[0] = 4.5f;
+	oss_delay[1] = 7.5f;
+	oss_delay[2] = 13.5f;
+	oss_delay[3] = 25.5f;
+
+	BMP180_ReadCalibrationData();
+}
+
+int32_t BMP180::BMP180_GetPressure()
+{
+	BMP180_ReadUCTemperature();
+	int UCPressure = BMP180_ReadUCPressure();
+	long pressure = 0;
+
+	long B6 =	(long)B5 - 4000;
+
+	long X1 = (B2 * ((B6 * B6) >> 12))>> 11;
+	long X2 = (AC2 * B6) >> 11;
+	long X3 = X1 + X2;
+	long B3 = (((AC1 * 4 + X3) << oversampling_setting) + 2) / 4;
+	X1 = (AC3 * B6) >> 13;
+	X2 = (B1 * ((B6 * B6) >> 12)) >> 16;
+	X3 = ((X1 + X2) + 2) >> 2;
+
+	
+	int32_t B4 = AC4 * (unsigned long)(X3 + 32768) >> 15;
+	uint32_t B7 = ((unsigned long)UCPressure - B3) * (50000 >> oversampling_setting);
+	if (B7 < 0x80000000){
+		pressure = (B7 * 2) / B4;
+	}
+	else {
+		pressure = (B7 / B4) *2;
+	}
+
+	X1 = (pressure >> 8) * (pressure >> 8);
+
+	X1 = (X1 * 3038) >> 16;
+	X2 = (-7357 * pressure) >> 16;
+
+	pressure = pressure + ((X1 + X2 + 3791) >> 4);
+	
+	return pressure;
 }
 
 int BMP180::BMP180_GetTemperature()
 {
 	// Get the uncompensated temp
-	UCTemp = BMP180_GetUCTemperature();
-
-	// The following is the Bosch Sensortec calibration algorithm for the BMP 180
-	long AC6 = (long)calibData[5];
-	long AC5 = (long)calibData[4];
-	long MC = (long)calibData[9];
-	long MD = (long)calibData[10];
-
-	long X1 = ((UCTemp-AC6) * AC5) >> 15;
-	long X2 = (MC << 11) / (X1 + MD);
-	long B5 = X1 + X2;
-	long T = ((B5 + 8)>>4);
+	UCTemp = BMP180_ReadUCTemperature();
+	
+	long X1 = (UCTemp - (int32_t)AC6) * (int32_t)AC5 >> 15;
+	long X2 = ((int32_t)MC << 11) / (X1 + (int32_t)MD);
+	B5 = X1 + X2;
+	long T = (B5 + 8) >>4;
 	return T;
 }
 
-int BMP180::BMP180_GetUCTemperature()
+int32_t BMP180::BMP180_ReadUCPressure()
+{
+	i2c_start_wait(DevBMPWrite);
+	i2c_write(0xF4);
+	i2c_write(0xF6 + (oversampling_setting << 6));
+	i2c_stop();
+
+	// Wait some time for the pressure to read
+	switch (oversampling_setting)
+	{
+		case 0:
+			_delay_ms(4.5);
+			break;
+		case 1:
+			_delay_ms(7.5);
+			break;
+		case 2:
+			_delay_ms(13.5);
+			break;	
+		case 3:
+			_delay_ms(25.5);
+			break;
+	}
+
+	i2c_start_wait(DevBMPWrite);
+	i2c_write(0xF6);
+
+	i2c_rep_start(DevBMPRead);
+	
+	volatile int32_t MSB = i2c_readAck();
+	volatile int32_t LSB = i2c_readAck();
+	volatile int32_t XLSB = i2c_readNak();
+
+	i2c_stop();
+
+	int32_t retVal = (((MSB << 16) & 0xFF0000) + ((LSB << 8) & 0xFF00) + (XLSB & 0xFF)) >> (8-oversampling_setting);
+
+	return retVal;
+}
+
+int BMP180::BMP180_ReadUCTemperature()
 {
 	int retVal = 0;
 	uint8_t MSB = 0;
@@ -72,26 +151,82 @@ int BMP180::BMP180_GetUCTemperature()
 	return retVal;
 }
 
-void BMP180::BMP180_GetCalibrationData()
+void BMP180::BMP180_ReadCalibrationData()
 {
 	// Tell BMP180 we want it to move its register pointer to the 0xAA register, the first calibration data
 	i2c_start_wait(DevBMPWrite);
 	i2c_write(0xAA);
 	i2c_rep_start(DevBMPRead);
 	
+
 	// Go through all 22 registers. Every time we read, the BMP180 moves to the next register
 	for (int i = 0; i < 11; i++)
 	{
-		uint8_t MSB = i2c_readAck();
-		uint8_t LSB;
+		int MSB = i2c_readAck();
+		int LSB;
 		if (i == 10)
 		LSB = i2c_readNak();
 		else
 		LSB = i2c_readAck();
 		
-		calibData[i] = 0;
-		calibData[i] |= (MSB<<8);
-		calibData[i] |= (LSB);
+		switch(i)
+		{
+			case 0:
+				AC1 = 0;
+				AC1 |= (MSB << 8);
+				AC1 |= (LSB);
+				break;
+			case 1:
+				AC2 = 0;
+				AC2 |= (MSB << 8);
+				AC2 |= (LSB);
+				break;
+			case 2:
+				AC3 = 0;
+				AC3 |= (MSB << 8);
+				AC3 |= (LSB);
+				break;
+			case 3:
+				AC4 = 0;
+				AC4 |= (MSB << 8);
+				AC4 |= (LSB);
+				break;
+			case 4:
+				AC5 = 0;
+				AC5 |= (MSB << 8);
+				AC5 |= (LSB);
+				break;
+			case 5:
+				AC6 = 0;
+				AC6 |= (MSB << 8);
+				AC6 |= (LSB);
+				break;
+			case 6:
+				B1 = 0;
+				B1 |= (MSB << 8);
+				B1 |= (LSB);
+				break;
+			case 7:
+				B2 = 0;
+				B2 |= (MSB << 8);
+				B2 |= (LSB);
+				break;
+			case 8:
+				MB = 0;
+				MB |= (MSB << 8);
+				MB |= (LSB);
+				break;
+			case 9:
+				MC = 0;
+				MC |= (MSB << 8);
+				MC |= (LSB);
+				break;
+			case 10:
+				MD = 0;
+				MD |= (MSB << 8);
+				MD |= (LSB);
+				break;
+		}
 	}
 
 	i2c_stop();
--- BMP180/BMP180.h
+++ BMP180/BMP180.h
@@ -9,7 +9,7 @@
 #ifndef __BMP180_H__
 #define __BMP180_H__
 
-#include "i2cmaster.h"
+#include "../i2cmaster.h"
 #include <util/delay.h>
 
 #define DevBMPRead 0xEF
@@ -18,16 +18,35 @@
 class BMP180
 {
 	// Our calibration data read from the BMP180 Sensor
-	int16_t calibData[11];
+	int16_t AC1;
+	int16_t	AC2;
+	int16_t AC3;
+	uint16_t AC4;
+	uint16_t AC5;
+	uint16_t AC6;
+	int16_t B1;
+	int16_t B2;
+	int16_t	MB;
+	int16_t MC;
+	int16_t MD;
+	//int16_t calibData[11];
+	short	oversampling_setting;
+
+	float oss_delay[4];
 
 	// Uncompensated temperature value from the BMP180 Sensor
 	int UCTemp;
+	int UCPressure;
+
+	long B5;
 
 	// Retrieves calibration data from the sensor (should be hard coded into the device)
-	void BMP180_GetCalibrationData();
+	void BMP180_ReadCalibrationData();
 	
 	// Polls the BMP180 device for the uncompensated temperature value
-	int16_t BMP180_GetUCTemperature();
+	int16_t BMP180_ReadUCTemperature();
+
+	int32_t	BMP180_ReadUCPressure();
 
 public:
 
@@ -35,6 +54,8 @@ public:
 	// the calibration data we collected at Setup to return a temperature in 
 	// 0.1 degrees C
 	int16_t BMP180_GetTemperature();
+
+	int32_t BMP180_GetPressure();
 	
 	// Initial setup that must be called... talks to the BMP180 sensor to get calibration data
 	void Setup();
